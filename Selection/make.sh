f78a4fe81195c849bc2f6c10e08f47d2608cf353
yuxiao <yuxiao.wang@cern.ch>
2021-09-07 19:12:57 +0200

    new branch

--- Selection/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-function run(){
-	        echo $1
-	        $1
-	        echo
-}
-run "g++ -o selection selection.cpp `root-config --cflags --glibs` -I. "
--- setup_root.sh
+++
@@ -1,3 +0,0 @@
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9/x86_64-slc6/setup.sh
-#source  /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.10/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
-source  /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.36/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
